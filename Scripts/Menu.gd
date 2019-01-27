233f1bf526dfc0e93d2c8d0fe79c7c6d1d7485f9
Conor Wallace <cwallace3421@gmail.com>
2019-01-27 14:00:33 +0000

    a bunch of changes

---
+++ Scripts/Menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var screen_id:int = 0
+
+func _process(delta):
+	if (Input.is_action_just_pressed("action")):
+		if (screen_id == 0):
+			screen_id += 1
+			$Main.visible = false
+			$Instructions.visible = true
+		elif (screen_id == 1):
+			get_tree().change_scene("res://Scenes/Beach.tscn")
+		
